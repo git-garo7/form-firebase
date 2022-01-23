fd2efc7a1373a363d57c9831d1f8209071538430
git-garo7 <joao.facul20@gmail.com>
2022-01-23 09:32:43 -0300

    o projeto form-front,agora integrado ao firebase.

---
+++ src/pages/index.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import Botao from "../components/Botao";
+import ColecaoCliente from "../components/firebasee/db/ColecaoCliente";
+import Formulario from "../components/Formulario";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
+
+export default function Home() {
+
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
+
+  const [cliente,setCliente] = useState<Cliente>(Cliente.vazio())
+
+  const [clientes,setClientes] = useState<Cliente[]>([])
+
+
+  const [visivel,setVisivel] = useState<'tabela' | 'form'>('tabela')
+
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
+
+  
+  function clienteSelecionado(cliente: Cliente) {
+    setCliente(cliente)
+    setVisivel('form')
+  }
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
+  }
+
+  async function salvarCliente(cliente: Cliente) {
+    await repo.salvar(cliente)
+    obterTodos()
+  }
+
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
+  }
+
+
+
+ 
+
+  return (
+    <div className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-l from-green-700 
+    text-black
+    `}>
+      <Layout titulo="Cadastro Simples">
+        {visivel === 'tabela' ? (
+          <>
+          <div className="flex justify-end">
+          <Botao cor="green" className="mb-4"
+           onClick={novoCliente}>
+             Novo Cliente
+             </Botao>
+          </div>
+          <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado} 
+          clienteExcluido={clienteExcluido} 
+       />
+       </>
+        ) : (
+          <Formulario 
+          cliente={cliente} 
+          clienteMudou={salvarCliente}
+          cancelado={() => setVisivel('tabela')}
+          />
+        )}
+        
+            
+      </Layout>
+    </div>
+  )
+}
